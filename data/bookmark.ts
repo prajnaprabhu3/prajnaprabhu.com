918100d59ce928d36425530cb374e8409cd25176
Prajna <prajnapprabhu3@gmail.com>
2023-07-27 23:58:45 +0530

    feat: bookmark page

---
+++ data/bookmark.ts
@@ -0,0 +1,56 @@
+export const bookmark = [
+  {
+    url: "https://evilmartians.com/chronicles/hands-on-webassembly-try-the-basics",
+  },
+  {
+    url: "https://blog.samaltman.com/idea-generation",
+  },
+  {
+    url: "https://lcamtuf.coredump.cx/photo_basics/?utm_source=hackernewsletter&utm_medium=email&utm_term=fav",
+  },
+  {
+    url: "https://ciechanow.ski/gps/",
+  },
+  // {
+  //   url: "https://www.kalzumeus.com/2020/10/09/four-years-at-stripe/",
+  // },
+  {
+    url: "https://www.kalzumeus.com/2012/08/13/doubling-saas-revenue/",
+  },
+  {
+    url: "https://longform.asmartbear.com/fulfillment/",
+  },
+  // {
+  //   url: "https://longform.asmartbear.com/startup-life/",
+  // },
+  {
+    url: "https://longform.asmartbear.com/extreme-questions/",
+  },
+  {
+    url: "https://longform.asmartbear.com/success-guaranteed/",
+  },
+  {
+    url: "https://longform.asmartbear.com/bad-advice/",
+  },
+  {
+    url: "https://garymarcus.substack.com/p/i-am-not-afraid-of-robots-i-am-afraid",
+  },
+  {
+    url: "https://garymarcus.substack.com/p/a-few-words-about-bullshit",
+  },
+  {
+    url: "https://chrisguillebeau.com/36-ways?utm_source=densediscovery&utm_medium=email&utm_campaign=newsletter-issue-227",
+  },
+  {
+    url: "https://www.iseed.vc/resources-founders",
+  },
+  {
+    url: "https://seldo.com/posts/you-will-never-be-a-full-stack-developer",
+  },
+  {
+    url: "https://singlefounderco.medium.com/",
+  },
+  {
+    url: "https://seldo.com/posts/databases_how_they_work_and_a_brief_history",
+  },
+];
