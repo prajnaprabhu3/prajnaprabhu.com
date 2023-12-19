26c3ff0b3d782273d062c5b99b48b283345401bd
Prajna <prajnapprabhu3@gmail.com>
2023-12-20 01:06:06 +0530

    fix: about content

--- src/components/about/data.ts
+++ src/components/about/data.ts
@@ -1,20 +1,18 @@
-import imgs from "@/../public/images/experience/hyper3.png";
-
 export const about = [
   {
     title: "Background",
     content:
-      "Born and raised in New Delhi, India. I've been into coding from a young age, and I'm passionate about many creative pursuits, including photography, design, and coding. This combination of interests is what led me to develop skills through guiltlessly exploring my passions and interests. I center my work on teaching/writing to empower, educate and elevate people.",
+      "I'm a fullstack developer from Karnataka, India who loves building minimal, modern and beautiful websites, mobile apps, and user interfaces. I personally like to work in the intersection product & engineering. I also enjoy playing basketball and doing random things including reading, music and lerning something new!",
   },
   {
     title: "Skills",
     content:
-      "I primarily work with React and Next.js ecosystem, and I've worked extensively with several tools such as TypeScript, Jest/Enzyme, Redux, Storybook, Chakra, Tailwind/SCSS, PostgreSQL, CircleCI, and much more. The web is an endless medium of opportunities, and I have yet to scratch the surface.",
+      "I enjoy working on React and all things JavaScript & TypeScript. I have worked with Reactjs, Nextjs, Redux, Zustand, Tailwindcss, Prisma, MongoDB, Expressjs, Nodejs in the past and constantly trying to get into depth of these technologies. And have explored a few other technologies. Currently, nerding out on native iOS development and frontend tooling!",
   },
   {
     title: "Now",
     content:
-      "Student looking for part/full-time internship opportunities or junior positions to work with startups on developing and designing web applications.",
+      "Student looking for part/full-time internship opportunities or junior positions to work with startups on developing products/softwares.",
   },
 ];
 
@@ -29,7 +27,7 @@ export const experience = [
       "Reactjs, Angularjs,TypeScript,Nodejs,MongoDB, Redux, Docker, CI/CD",
       // "Serve as an intermediary bridging the gap between the user and the backend developers, and tweak tools and platforms",
     ],
-    img: "/../public/images/experience/mob.png",
+    img: "/images/experience/mob.png",
   },
   {
     id: 2,
@@ -37,10 +35,10 @@ export const experience = [
     designation: "Frontend Engineer",
     timeline: "December 2022 - April 2023",
     details: [
-      "Ideate, Plan and implement the user interface systems of main web app, software programs.",
+      "Ideate, Plan and implement the user interface systems of main web app",
       "Nextjs, TypeScript, Redux, MongoDB ",
       // "Serve as an intermediary bridging the gap between the user and the backend developers, and tweak tools and platforms",
     ],
-    img: "/../public/images/experience/hyper3.png",
+    img: "/images/experience/hyper3.png",
   },
 ];
